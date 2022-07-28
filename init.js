24dbfca6fd097484e8a06f8a2081995c7c523e85
weisiwu <siwu.wsw@autonavi.com>
2022-07-28 10:50:38 +0800

    chore: 2022年07月28日问题

--- init.js
+++ init.js
@@ -1,6 +1,12 @@
 const { exec } = require('child_process');
+const fs = require('fs');
 const time = (new Date());
 const month = time.getMonth() + 1;
+
+const folder = `./${time.getFullYear()}年${month < 10 ? `0${month}` : month}月`;
 const date = `${time.getFullYear()}年${month < 10 ? `0${month}` : month}月${time.getDate()}日`;
 
-exec(`cp QA.template ${date}.md`);
+if (!fs.existsSync(folder)) {
+    exec(`mkdir ${folder}`);
+}
+exec(`cp QA.template ${folder}/${date}.md`);
