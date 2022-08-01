296dfa1a53eb653bf0d74683e987294c643cde43
weisiwu <siwu.wsw@autonavi.com>
2022-08-01 18:05:26 +0800

    chore: 添加问题

--- publish.sh
+++ publish.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
 # 一键发布提交，并拿到生成的文档地址
+
+date=$(date "+%Y%m%d" )
+
+type git
+if [ $? -eq 0 ]; then
+    git add .
+    git commit -m "chore: 添加${xx}问题"
+    if [ $? -eq 0 ]; then
+        git push
+    fi
+fi
+
+if [ $? -eq 0 ]; then
+    echo '推送成功'
+fi
